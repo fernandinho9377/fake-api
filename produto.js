0372c166662d85a06c5b4bdd78960e8c019f8966
Fernando da Rocha Moreira <fernando.r.moreira@aluno.senai.br>
2022-12-10 20:20:18 -0300

    api-fake, lista de produtos

---
+++ produto.js
@@ -0,0 +1,5 @@
+[
+    {"nome":"Camisa1","img":"C:\Users\Fernandinho\Desktop\JAVA CURSO SENAI\fake-api\img\camisa1","valor":41.6},
+    {"nome":"camisa2","img":"C:\Users\Fernandinho\Desktop\JAVA CURSO SENAI\fake-api\img\camisa2","valor":42.5},
+    
+]
