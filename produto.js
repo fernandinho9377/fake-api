c56bd44a461b5f60d401179135a5481fecde96dd
fernandinho9377 <115050109+fernandinho9377@users.noreply.github.com>
2022-12-10 22:06:56 -0300

    Update produto.js

--- produto.js
+++ produto.js
@@ -1,5 +1,5 @@
 [
-    {"nome":"camisa1","img":"https://fernandinho9377.github.io/fake-api/img/camisa1.jpg","valor":39.90},
-    {"nome":"camisa2","img":"https://fernandinho9377.github.io/fake-api//img/camisa2.jpg","valor":40.90},
+    {"nome":"camisa1","img":"https://fernandinho9377.github.io/fake-api/img/camisa1.png","valor":39.90},
+    {"nome":"camisa2","img":"https://fernandinho9377.github.io/fake-api//img/camisa2.png","valor":40.90},
     
 ]
