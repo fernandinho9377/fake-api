bdd0166c517b7f08adae3beaac0537d5e981e932
Fernando da Rocha Moreira <fernando.r.moreira@aluno.senai.br>
2022-12-12 16:09:16 -0300

    alteração nos endereços

--- produto.js
+++ produto.js
@@ -1,11 +1,11 @@
 [
-    {"nome":"Camisa1","img":"C:\Users\Fernandinho\Desktop\JAVA CURSO SENAI\fake-api\img\camisa1.png","valor":42.50},
-    {"nome":"camisabranca","img":"C:\Users\Fernandinho\Desktop\JAVA CURSO SENAI\fake-api\img\camisabranca.png","valor":42.5},
-    {"nome":"camisavermelha","img":"C:\Users\Fernandinho\Desktop\JAVA CURSO SENAI\fake-api\img\camisavermelha.png","valor":42.5},
-    {"nome":"camisacinza","img":"C:\Users\Fernandinho\Desktop\JAVA CURSO SENAI\fake-api\img\camisacinza.png","valor":42.5},
-    {"nome":"camisamangalonga","img":"C:\Users\Fernandinho\Desktop\JAVA CURSO SENAI\fake-api\img\camisamangalonga.png","valor":60.50},
-    {"nome":"camisaxadrez","img":"C:\Users\Fernandinho\Desktop\JAVA CURSO SENAI\fake-api\img\camisaxadrez.png","valor":69.50},
-    {"nome":"camisasocial","img":"C:\Users\Fernandinho\Desktop\JAVA CURSO SENAI\fake-api\img\camisasocial.png","valor":79.90},
-    {"nome":"moletom","img":"C:\Users\Fernandinho\Desktop\JAVA CURSO SENAI\fake-api\img\moletom.png","valor":119.90}
+    {"nome":"Camisa1","img":"C:/Users/Fernandinho/Desktop/JAVA CURSO SENAI/fake-api/img/camisa1.png","valor":42.50},
+    {"nome":"camisabranca","img":"C:/Users/Fernandinho/Desktop/JAVA CURSO SENAI/fake-api/img/camisabranca.png","valor":42.5},
+    {"nome":"camisavermelha","img":"C:/Users/Fernandinho/Desktop/JAVA CURSO SENAI/fake-api/img/camisavermelha.png","valor":42.5},
+    {"nome":"camisacinza","img":"C:/Users/Fernandinho/Desktop/JAVA CURSO SENAI/fake-api/img/camisacinza.png","valor":42.5},
+    {"nome":"camisamangalonga","img":"C:/Users/Fernandinho/Desktop/JAVA CURSO SENAI/fake-api/img/img/camisamangalonga.png","valor":60.50},
+    {"nome":"camisaxadrez","img":"C:/Users/Fernandinho/Desktop/JAVA CURSO SENAI/fake-api/img/camisaxadrez.png","valor":69.50},
+    {"nome":"camisasocial","img":"C:/Users/Fernandinho/Desktop/JAVA CURSO SENAI/fake-api/img/camisasocial.png","valor":79.90},
+    {"nome":"moletom","img":"C:/Users/Fernandinho/Desktop/JAVA CURSO SENAI/fake-api/img/moletom.png","valor":119.90}
     
 ]
