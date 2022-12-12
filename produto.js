b335d0755f432d022654f95f5f762da9cab412e1
Fernando da Rocha Moreira <fernando.r.moreira@aluno.senai.br>
2022-12-12 11:32:50 -0300

    mais camisas

--- produto.js
+++ produto.js
@@ -1,5 +1,11 @@
 [
-    {"nome":"Camisa1","img":"C:\Users\Fernandinho\Desktop\JAVA CURSO SENAI\fake-api\img\camisa1","valor":41.6},
-    {"nome":"camisa2","img":"C:\Users\Fernandinho\Desktop\JAVA CURSO SENAI\fake-api\img\camisa2","valor":42.5},
+    {"nome":"Camisa1","img":"C:\Users\Fernandinho\Desktop\JAVA CURSO SENAI\fake-api\img\camisa1","valor":42.50},
+    {"nome":"camisabranca","img":"C:\Users\Fernandinho\Desktop\JAVA CURSO SENAI\fake-api\img\camisabranca","valor":42.5},
+    {"nome":"camisavermelha","img":"C:\Users\Fernandinho\Desktop\JAVA CURSO SENAI\fake-api\img\camisavermelha","valor":42.5},
+    {"nome":"camisacinza","img":"C:\Users\Fernandinho\Desktop\JAVA CURSO SENAI\fake-api\img\camisacinza","valor":42.5},
+    {"nome":"camisamangalonga","img":"C:\Users\Fernandinho\Desktop\JAVA CURSO SENAI\fake-api\img\camisamangalonga","valor":60.50},
+    {"nome":"camisaxadrez","img":"C:\Users\Fernandinho\Desktop\JAVA CURSO SENAI\fake-api\img\camisaxadrez","valor":69.50},
+    {"nome":"camisasocial","img":"C:\Users\Fernandinho\Desktop\JAVA CURSO SENAI\fake-api\img\camisasocial","valor":79,90},
+    {"nome":"moletom","img":"C:\Users\Fernandinho\Desktop\JAVA CURSO SENAI\fake-api\img\moletom","valor":119.90}
     
 ]
